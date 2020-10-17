569d917da4a4ec28ba3eceb0b1d8ce5067dc2dfa
Daishi Nakajima <nakaji.dayo@gmail.com>
2020-10-18 00:44:58 +0900

    chapter 2

---
+++ 1/1d.sml
@@ -0,0 +1,56 @@
+type id = string;
+
+datatype binop = Plus | Minux | Times | Div;
+
+datatype stm = CompoundStm of stm * stm
+             | AssignStm of id * exp
+             | PrintStm of exp list
+     and exp = IdExp of id
+             | NumExp of int
+             | OpExp of exp * binop * exp
+             | EseqExp of stm * exp;
+
+(********************)
+
+type key = string;
+datatype 'a tree = Leaf | Tree of 'a tree * key * 'a * 'a tree * bool;
+
+val empty = Leaf;
+
+fun insert (key, v, Leaf) = Tree(Leaf, key, v, Leaf, false)
+  | insert (key, value, Tree(l, k, v, r, isR)) =
+    if key < k
+    then Tree(insert(key, value, l), k, v, r, isR)
+    else if key > k
+    then Tree(l, k, v, insert(key, value, r, isR))
+    else Tree(l, key, value, r, isR);
+
+fun balance (3)
+
+
+fun member (key, Leaf) = false
+  | member (key, Tree(l, k, _, r)) =
+    if key < k
+    then member(key, l)
+    else if key > k
+    then member(key, r)
+    else true
+
+fun lookup (key, Leaf) = NONE
+  | lookup (key, Tree(l, k, v, r)) =
+    if key < k
+    then lookup(key, l)
+    else if key > k
+    then lookup(key, r)
+    else SOME v;
+
+fun size Leaf = 0
+  | size (Tree(l, k, v, r)) = 1 + size(l) + size(r);
+
+fun left (Tree(l, _, _, _)) = l
+fun right (Tree(_, _, _, r)) = r
+
+
+(************)
+val t1 = insert("t", 2, insert("s", 2, insert("b", 2, insert("f", 2, insert("i", 2, insert("p", 4, insert("s", 3, insert("t", 1, empty))))))));
+val t2 = insert("h", 2, insert("g", 2, insert("f", 2, insert("e", 2, insert("d", 2, insert("c", 4, insert("b", 3, insert("a", 1, empty))))))));
---
+++ 2/Nfa.hs
@@ -0,0 +1,182 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE TypeApplications      #-}
+
+module NFA where
+
+import           Control.Lens    ((^.))
+import           Control.Monad   (msum, (>>))
+import           Data.Foldable   (toList)
+import           Data.List       (findIndex, nubBy)
+import qualified Data.Map.Strict as M
+import           Data.Maybe      (catMaybes, fromMaybe)
+import           Data.Monoid
+import           Data.Sequence   (Seq (..), findIndexL, fromList, index, (|>))
+import           Debug.Trace
+import           GHC.Records     (HasField (getField))
+
+
+
+anyChar = ['0'..'z']
+lower = ['a'..'z']
+digit = ['0'..'9']
+
+data Node = Node
+  { symbol :: Int
+  , final  :: Maybe String
+  } deriving (Show, Eq)
+
+data State a = State
+  { node  :: a
+  , edges :: [([Char], State a)]
+  } deriving (Show) --
+
+-- foldMapOnce :: (Eq a, Monoid m) => (a -> m) -> State a -> m
+-- foldMapOnce f (State n s) =
+--   go [] f (State n s)
+--   where
+--     go used f (State n s)
+--       | n `elem` used = mempty
+--       | otherwise = f n <> mconcat (go (n:used) f . snd <$> s)
+
+-- findOnce :: (Eq a) => (a -> Bool) -> State a -> Maybe a
+-- findOnce p = getFirst . foldMapOnce (\ x -> First (if p x then Just x else Nothing))
+
+findStateBy :: Eq a => (a -> Bool) -> State a -> Maybe (Maybe [Char], State a)
+findStateBy f s = go Nothing [] s
+  where
+    go c used s@(State n' es)
+      | n' `elem` used = Nothing
+      | f n' = Just (c, s)
+      | otherwise =
+          case es of
+            [] -> Nothing
+            _  -> msum $ map (\(c, s) -> go (Just c) (n':used) s) es
+
+nfaState s mf es = State (Node s mf) es
+
+s8 = nfaState 8 (Just "ID")
+     [ ("", nfaState 6 Nothing
+            [ ((lower ++ digit), nfaState 7 Nothing
+                                 [ ("", s8)
+                                 ]
+              )
+            ]
+       )
+     ]
+s13 = nfaState 13 (Just "NUM")
+     [ ("", nfaState 11 Nothing
+            [ (digit, nfaState 12 Nothing
+                                 [ ("", s13)
+                                 ]
+              )
+            ]
+       )
+     ]
+r = nfaState 1 Nothing
+    [ ("i", nfaState 2 Nothing
+            [ ("f", nfaState 3 (Just "IF") [])
+            ]
+      )
+    , ("", nfaState 14 Nothing
+           [ (anyChar, nfaState 15 (Just "ERROR") [])
+           ]
+      )
+    , ("", nfaState 4 Nothing
+           [ (lower, nfaState 5 Nothing
+               [ ("", s8
+                 )
+               ]
+             )
+           ]
+      )
+    , ("", nfaState 9 Nothing
+           [ (digit, nfaState 10 Nothing
+               [ ("", s13
+                 )
+               ]
+             )
+           ]
+      )
+    ]
+
+
+pp :: (Show a, Show b, Eq b, HasField "symbol" a b) => State a -> IO ()
+pp = pp' [] 0 ""
+  where
+    pp' used lv c (State n es)
+      | getField @"symbol" n `elem` used = putStrLn $ concat [take (lv*2) $ repeat ' ', "(", show $ getField @"symbol" n, ")"]
+      | otherwise = do
+          putStrLn $ concat [take (lv*2) $ repeat ' ', "[", show $ getField @"symbol" n, "]", ps c, " ==> "]
+          mapM_ (uncurry (pp' (getField @"symbol" n:used) (lv + 1))) es
+      where
+        ps "" = "ε"
+        ps x  = x
+
+showSymbols :: Show a => [State a] -> IO ()
+showSymbols = mapM_ (print . node)
+
+-- Nothing: e
+edge c s =
+  map snd $ filter (match c) $ edges s
+  where
+    match Nothing ("", _) = True
+    match (Just c) (x, _) = c `elem` x
+    match _ _             = False
+
+closure :: Eq a => [State a] -> [State a]
+closure s =
+  let t = nubBy (\a b -> node a == node b) $ s ++ (concat $ edge Nothing <$> s)
+  in if eqSymbols t s
+     then t
+     else closure t
+
+eqSymbols xs ys = map node xs == map node ys
+
+dfaEdge :: Eq a => Char -> [State a] -> [State a]
+dfaEdge c s =
+  closure $ concatMap (edge (Just c)) s
+
+runNFA cs s = go cs (closure [s])
+  where
+    go [] s'     = s'
+    go (c:cs) s' = go cs (dfaEdge c s')
+
+----
+
+
+data DFANode = DFANode
+  { symbol :: [Int]
+  , final  :: Maybe String
+  } deriving (Show, Eq)
+
+--  NFA -> DFA変換
+-- 写経。直接State DFANodeにreduceできる？
+
+type Intermediate = (Seq [State Node], [(Int, Char, Int)])
+
+trans s =
+  step (fromList [closure [s]], []) 0 0
+  where
+    step :: Intermediate -> Int -> Int -> Intermediate
+    step x j p
+      | j > p = x
+      | otherwise =
+        let (x', j', p') = foldl (\acc x -> go x acc) (x, j, p) anyChar
+        in step x' (j' + 1) p'
+    go :: Char -> (Intermediate, Int, Int) -> (Intermediate, Int, Int)
+    go c ((state, trans), j, p) =
+      let e = dfaEdge c (state `index` j)
+      in case findIndexL (shallowEq $ e) state of
+        Just idx ->
+          ((state, (j, c, idx):trans), j, p)
+        Nothing ->
+          ((state |> e, (j, c, p+1):trans), j, p+1)
+    shallowEq x y = map (getField @"symbol" . node) x == map (getField @"symbol" . node) y
+
+reduceTrans xs = M.fromListWith (++) $ map (\(f, c, t) -> ((f, t), [c])) xs
+
+test (state, trans) = do
+  mapM_ (\(i, s) -> print i >> showSymbols s) $ zip [0..] (toList state)
+  mapM_ print (M.toList $ reduceTrans trans)
